29d4255de7aa33df537be9ee46c67dc927d33fa9
Marko <markomiljkovic2@gmail.com>
2019-11-19 00:24:18 +0100

    added login page, added dot-env

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const dotenv = require('dotenv');
+const { ApolloServer } = require('apollo-server-express');
 const typeDefs = require('./schemas');
 const resolvers = require('./resolvers');
+
+dotenv.config();
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 server.applyMiddleware({ app });
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen({ port: PORT }, () =>
     console.log(`🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`)
