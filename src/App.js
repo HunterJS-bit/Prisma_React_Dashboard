5a0a2e6ae1ef0a84cfbb94c3aaf3459b9c8c20fc
Marko <markomiljkovic2@gmail.com>
2019-11-20 00:27:33 +0100

    added apollo client, added login page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import Home from './components/Home';
 import Header from './components/common/Header';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+});
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Home />
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Header />
+          <Home />
+        </div>
+      </ApolloProvider>
     </BrowserRouter>
   );
 }
--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+
+    logIn = (e) => {
+        e.preventDefault();
+        console.log('Submiting form')
+    }
+
     render() {
-        return <h1>Login page</h1>
+        return (
+            <div className="wrap">
+                <form onSubmit={this.logIn}>
+                    <div >
+                        <h3>Login Form</h3>
+                        <p>Login to access your dashboard</p>
+                    </div>
+                    <div >
+                        <input type="text" className="form-input" placeholder="email@example.com" />
+                    </div>
+                    <div>
+                        <input type="password" className="form-input" placeholder="password" />
+                    </div>
+                    <div>
+                        <button className="form-button" type="submit">Login</button>
+                    </div>
+                    <div>
+                        Don't have an account? <a href="/#">Sign Up</a>
+                    </div>
+                </form>
+            </div >
+        );
     }
 }
 
