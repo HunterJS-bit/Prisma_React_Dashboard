a949638ef05a08eef7ac91b9e65fc20f6aee523f
Marko <markomiljkovic2@gmail.com>
2019-12-07 01:42:55 +0100

    added prisma orm

---
+++ server/data/seed.js
@@ -0,0 +1,22 @@
+const { prisma } = require('../generated/prisma-client');
+const users = require('./users.json');
+const bcrypt = require('bcryptjs');
+/*
+const db = new Prisma({
+    endpoint: 'http://localhost:4466'
+});
+*/
+// setup seed 
+const setup = async () => {
+
+    const allUsers = users.map(async (user, index) => {
+        const userPassword = await bcrypt.hash(user.password, 10);
+        await prisma.createUser({ email: user.email, password: userPassword });
+    });
+    await Promise.all(allUsers);
+
+    // await prisma.createUser({ email: 'markoo', password: 'milos' })
+    console.log('Doneeee');
+};
+
+setup();
