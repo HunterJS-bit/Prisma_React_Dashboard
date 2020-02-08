e699987204c8b767ecf9f4386cd8157eb49c33b8
Marko <markomiljkovic2@gmail.com>
2020-02-08 18:09:19 +0100

    fix: google analytics refresh token not fetched

--- server/services/google-util.js
+++ server/services/google-util.js
@@ -9,8 +9,9 @@ const callbackURL = 'http://localhost:3000/google/return';
 const oauth2Client = new google.auth.OAuth2(clientID, clientSecret, callbackURL);
 
 const url = oauth2Client.generateAuthUrl({
-    access_type: 'online',
-    scope: 'https://www.googleapis.com/auth/analytics.readonly'
+    access_type: 'offline',
+    scope: 'https://www.googleapis.com/auth/analytics.readonly',
+    prompt: 'consent'
 });
 
 const analytics = google.analytics({
@@ -19,29 +20,36 @@ const analytics = google.analytics({
 });
 
 
-oauth2Client.on('tokens', (tokens) => {
-	console.log('OVdeee sammmm ');
- 	console.log(tokens);
-});
-
-
 const getAnalytics = async () => {
-	const viewId = process.env.viewID;
-	console.log('Getting google analytics');
-
-	console.log('---------------------')
-	try {
-		const { data } = await analytics.data.ga.get({
-			ids: 'ga:' + viewId,
-		    auth: oauth2Client,
-		    'start-date': '30daysAgo',
-	    	'end-date': 'today',
-	    	'metrics': 'ga:pageviews'
+	const credentials =	Object.keys(oauth2Client.credentials).length;
+	if (!credentials) {
+		oauth2Client.setCredentials({
+		  refresh_token: process.env.analyticsRefreshToken
 		});
+	}
+	console.log('Getting google analytics');
+	try {
+		const { data } = await analytics.management.profiles.list({
+				accountId: '~all',
+				webPropertyId: '~all',
+				auth: oauth2Client
+		});
+		const view = { id: data.items[0].id, website: data.items[0].websiteUrl }
+		const { data: result } = await analytics.data.ga.get({
+			ids: 'ga:' + view.id,
+		    auth: oauth2Client,
+		    'start-date': '80daysAgo',
+	    	'end-date': 'yesterday',
+	    	'metrics': 'ga:hits'
+		});
+		console.log('Analitikaaa ');
+		console.log(result.rows);
+
 	} catch (e) {
         console.log('Errorr', e);
+        console.log(url);
+   
     }
-	// console.log(data);
 
 }
 
