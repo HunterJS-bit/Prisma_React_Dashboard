fc44fb8d4724c8287ed6ddc4c978a70c069e75bd
Marko <markomiljkovic2@gmail.com>
2020-02-16 01:14:41 +0100

    fix: prevent graph from being drawn multiple times

--- client/src/components/common/Chart.js
+++ client/src/components/common/Chart.js
@@ -14,43 +14,49 @@ const Chart = (props) => {
 
 	useEffect(
         () => {
-        	
- 			var svg = d3.select("#graph").append("svg")
+
+        	const childNode = chartRef.current.children.length;
+
+        	if (!childNode) {
+        		const svg = d3.select("#graph").append("svg")
+        		.attr("class", "graph")
 			    .attr("width", width + margin.left + margin.right)
 			    .attr("height", height + margin.top + margin.bottom)
-			  .append("g")
-			    .attr("transform", 
-			          "translate(" + margin.left + "," + margin.top + ")");
+				.append("g")
+				.attr("transform", 
+				        "translate(" + margin.left + "," + margin.top + ")");
 
-			var x = d3.scaleBand()
+				const x = d3.scaleBand()
 			          .range([0, width])
 			          .padding(0.1);
-			var y = d3.scaleLinear()
+				const y = d3.scaleLinear()
 			          .range([height, 0]);
 
-			 // Scale the range of the data in the domains
-			x.domain(chartData.map(function(d) { return d.date; }));
-			y.domain([0, d3.max(chartData, function(d) { return d.count; })]);
+			 	// Scale the range of the data in the domains
+				x.domain(chartData.map(function(d) { return d.date; }));
+				y.domain([0, d3.max(chartData, function(d) { return d.count; })]);
 
-			// add the x Axis
-			svg.append("g")
+				// add the x Axis
+				svg.append("g")
 			      .attr("transform", "translate(0," + height + ")")
 			      .call(d3.axisBottom(x));
 
-			// add the y Axis
-			svg.append("g")
+				// add the y Axis
+				svg.append("g")
 				      .call(d3.axisLeft(y));
 
 
-			 // append the rectangles for the bar chart
-			 svg.selectAll(".bar")
-			    .data(chartData)
-			    .enter().append("rect")
-			      .attr("class", "bar")
-			      .attr("x", function(d) { return x(d.date); })
-			      .attr("width", x.bandwidth())
-			      .attr("y", function(d) { return y(d.count); })
-			      .attr("height", function(d) { return height - y(d.count); });
+				// append the rectangles for the bar chart
+				svg.selectAll(".bar")
+			    	.data(chartData)
+			    	.enter().append("rect")
+			      	.attr("class", "bar")
+			      	.attr("x", function(d) { return x(d.date); })
+			      	.attr("width", x.bandwidth())
+			      	.attr("y", function(d) { return y(d.count); })
+			      	.attr("height", function(d) { return height - y(d.count); });
+
+        	}
         
     });
 
