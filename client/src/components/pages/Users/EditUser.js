410a9170b89aad58ff22baaead35eef3892cf407
Marko <markomiljkovic2@gmail.com>
2020-03-15 18:11:32 +0100

    feat(editUser) update edit user form, started organizing code, updated git ignore

--- client/src/components/pages/Users/EditUser.js
+++ client/src/components/pages/Users/EditUser.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, Input, Button, Select } from 'antd';
 import gql from 'graphql-tag';
 import { useQuery } from '@apollo/react-hooks';
 
@@ -7,9 +8,23 @@ const GET_USER = gql`
     	user(id: $id) {
       		id
       		name
+      		role
+      		email
+      		profileImage
     }
   }
 `;
+
+const { Option } = Select;
+
+const formItemLayout = {
+  labelCol: {
+    span: 6,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+};
 
 const EditUser = (props) => {
 
@@ -22,7 +37,46 @@ const EditUser = (props) => {
   	if (error) return `Error! ${error.message}`;
   	console.log(data);
 
-	return (<h1> Edit User </h1>);
+  	const onFinish = values => {
+	    console.log('Received values of form: ', values);
+	};
+
+	return (<Form name="dynamic_rule">
+		<h1> Edit User </h1>
+         <Form.Item label="Name">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+           <Form.Item name="input-number" label="Email">
+          <Input placeholder="input placeholder" />
+        </Form.Item>
+             <Form.Item
+	        name="select"
+	        label="Select"
+	        hasFeedback
+	        rules={[
+	          {
+	            required: true,
+	            message: 'Please select role!',
+	          },
+	        ]}
+	      >
+	        <Select placeholder="Please select a country">
+	          <Option value="china">Admin</Option>
+	          <Option value="usa">Constribtor</Option>
+	          <Option value="usa">User</Option>
+	        </Select>
+	      </Form.Item>
+         <Form.Item
+        wrapperCol={{
+          span: 12,
+          offset: 6,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+		</Form>);
 }
 
 export default EditUser;
